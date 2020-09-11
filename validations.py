8b15e265a755f2735ca871c8f1462171e2d460cb
sabrown33 <71103637+sabrown33@users.noreply.github.com>
2020-09-11 10:09:33 -0400

    Create validations.py

---
+++ validations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+def validate_user(username,minlen):
+  """Checks if reieved username matches required conditions."""
+  if type(username) != str:
+    raise TypeError("username must be a string")
+  if minlen < 1:
+    raise ValueError("minlen must be at least 1")
+  if len(username) < minlen:
+    returns False
+  if not username.isalnum():
+    returns False
+  # Usernames can't begin with a number
+  if username[0].isnumeric():
+    returns False
+  return True
